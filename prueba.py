7119f4381525fce29d6ebf7d5c01061459469b30
junnchy <juanichacho@gmail.com>
2019-08-27 21:04:53 -0300

    modificacion de funciones

--- prueba.py
+++ prueba.py
@@ -1,7 +1,14 @@
 import pyRofex
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas import DataFrame
 from _datetime import datetime
+import sys
+from datetime import datetime
+import pymongo
+from pymongo import MongoClient
+import json
+from pymongo.errors import ConnectionFailure
 
 
 pyRofex.initialize(user='juanichacho2557',
@@ -15,6 +22,8 @@ def buscarInstrumentos(c):
     l = len(c)
 
     a = getInstrumentos()
+
+    print(type(a))
 
     b = a['instruments']
 
@@ -57,7 +66,14 @@ def getListaSimbolos():
 def armarListadoDeTrades(a):
     trades = pyRofex.get_trade_history(ticker=a,
                                        start_date='2019-08-07',
-                                       end_date='2019-08-25')
+                                       end_date='2019-08-27')
+
+    return trades
+
+def armarTrades(a):
+    trades = pyRofex.get_trade_history(ticker=a,
+                                       start_date='2019-08-07',
+                                       end_date='2019-08-27')
 
     return trades
 
@@ -67,12 +83,48 @@ def graficar(t):
     plt.gcf().autofmt_xdate()
     plt.show()
 
+def dbManager():
+    "Connect to MongoDB"
+    try:
+        c = MongoClient(
+            host="localhost",
+            port=27017
+        )
+        print("Connected successfully")
+    except ConnectionFailure:
+        sys.stderr.write("Could not connet to MongoDB")
+        sys.exit(1)
+
+    db = c['rofex']
+
+    return db
+
+def cargarDolar(db, trade):
+    #Reformar para que tome cualquier mercado
+    print(DataFrame(trade['trades']))
+    for i in trade['trades']:
+        dolar_doc = {
+            'price': i['price'],
+            'size': i['size'],
+            'datatime':i['datetime'],
+            'servertime':i['servertime']
+        }
+
+        db.DOSep19.insert_one(dolar_doc)
+        print ('Carga exitosa en el documento: ', dolar_doc)
+
 def main():
     #buscarInstrumentos('SO')
-    #print(armarListadoDeTrades('SOJ.ROSNov19'))
+    a = armarListadoDeTrades('DOSep19')
+
     #graficar(armarListadoDeTrades('SOJ.ROSNov19'))
     #getInstrumentos()
     #listarInstrumentos()
-    getListaSimbolos()
+    #db = dbManager()
+    #cargarDolar(db, a)
+
+    #buscarInstrumentos('DOSep19')
+
+    #print(DataFrame(getListaSimbolos()))
 
 main()
---
+++ tpandas/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/juanchi/miniconda3/envs/tpandas/lib"
+XML2_LIBS="-lxml2 -L/home/juanchi/miniconda3/envs/tpandas/lib -lz -L/home/juanchi/miniconda3/envs/tpandas/lib -llzma -lpthread -L/home/juanchi/miniconda3/envs/tpandas/lib  -lm "
+XML2_INCLUDEDIR="-I/home/juanchi/miniconda3/envs/tpandas/include/libxml2 -I/home/juanchi/miniconda3/envs/tpandas/include"
+MODULE_VERSION="xml2-2.9.9"
+
